ed5ce80af8425082176b06455a49cf332d70d760
aleem ahmed <61424530+aleem-ahmed@users.noreply.github.com>
2021-01-03 19:47:54 -0500

    Update PageService.js

--- client/src/services/PageService.js
+++ client/src/services/PageService.js
@@ -2,16 +2,6 @@
 import axios from 'axios'
 
 
-const authAxios = async () => {
-    return axios.create({
-        baseURL: '/pages',
-        headers: {
-            
-        }
-    })
-}
-
-
 async function s_home() {
     try {
         const response = await axios.create().get('/pages/')
