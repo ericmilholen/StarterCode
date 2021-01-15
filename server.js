a0c99f277c7f1727b8d42b278ad3e2a16c85d2fd
Eric Milholen <ericmilholen@gmail.com>
2021-01-15 13:44:54 -0500

    Added About Page on the Route

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express')
 
 // [REQUIRE] Personal // 
 const p_ = require('./s-routes/pages')
+const p_about = require('./s-routes/pages/about')
+
 
 
 // [EXPRESS] //
@@ -17,15 +19,7 @@ const port = 5000
 app.use(cors())
 
 app.use('/pages', p_)
-
-app.get('/pages/about', (req, res) => {
-    res.send('About!')
-})
-
-
-app.get('/pages/dashboard', (req, res) => {
-    res.send('Page!')
-})
+app.use('/pages/about', p_about)
 
 
 app.listen(port, () => {
